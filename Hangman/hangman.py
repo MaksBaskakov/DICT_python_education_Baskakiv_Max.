2ec8f51b1b29c880835ee7d0bfd6b617782d0c83
MaksBaskakov <m.a.baskakov@student.khai.edu>
2021-10-25 10:44:52 +0300

    Hangman 3 etap

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -9,16 +9,29 @@ The will start soon
 
 """)
 import random
-c=['dog', 'tiger', 'zebra' ]
+#c=['dog', 'tiger', 'zebra' ]
 
 
 print("Guess the word:>")
 
 
 
+#def answers():
+    #answer = str(input())
+    #if answer == (c[0]):
+        ##else:
+        #print("You lost")
+       #answers()
+#answers()
+
+s = ["phyton","java","javascript","php",]
+
+
+a = random.choice(s)
+
 def answers():
-    answer = str(input())
-    if answer == (c):
+    answer= str(input())
+    if answer == (a):
         print("You win")
     else:
         print("You lost")
