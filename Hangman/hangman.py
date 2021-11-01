91ac5f50d34818c65bf0ca4d08c6a6cb3cd82ca1
MaksBaskakov <m.a.baskakov@student.khai.edu>
2021-11-01 20:16:48 +0200

    5 stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -24,27 +24,39 @@ import random
        #answers()
 #answers()
 
-s = ["phyton","java","javascript","php",]
-
-
-a = random.choice(s)
-
-b=a[0]+a[1]+a[2]
 
 
 
 
 
-print("Guess the word:>"+ b)
-
-
-
-def answers():
-    answer= str(input())
-    if answer == (a):
-        print("You win")
+w = ['python', 'javascript', 'php', 'java']
+answer_prog = random.choice(w)  #python
+word_list = list(answer_prog)   # [p,y,t,h,o,n]
+user_word_list_null = "-" * len(answer_prog)    # ------
+user_list = list(user_word_list_null)    # [-,-,-,-,-,-]
+print(user_word_list_null)
+count = 0
+while count != 8:
+    count += 1
+    answer_user = str(input('Input a letter:'))
+    if answer_user in word_list:
+        if word_list.count(answer_user) >= 2:
+            index = word_list.index(answer_user)
+            user_list[index] = answer_user
+            word_list[index] = "-"
+        index = word_list.index(answer_user)
+        user_list[index] = answer_user
     else:
-        print("You lost")
-        answers()
-answers()
+        print("That letter doesn't appear in the word")
+    print(''.join(user_list))
+print("Thanks for playing!")
+print("We'll see how well you did in the next stage")
+
+
+
+
+
+
+
+
 
