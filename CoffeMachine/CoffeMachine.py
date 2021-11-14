5bcc4d1f685e34cefbde9d93f7d0a2d17bf91087
MaksBaskakov <m.a.baskakov@student.khai.edu>
2021-11-14 15:51:35 +0200

    2-st Stage

--- CoffeMachine/CoffeMachine.py
+++ CoffeMachine/CoffeMachine.py
@@ -8,12 +8,12 @@ Coffee is ready!""")
 coffee_water = 200
 coffee_milk = 50
 coffee_beans = 15
-# count_cups = int(input("Write how many cups of coffee you will need:"))
-# water = count_cups * coffee_water
-# milk = count_cups * coffee_milk
-# beans = count_cups * coffee_beans
-# print('For ' + str(count_cups) + ' cups of coffee you will need:')
-# print(str(water) + ' ml of water')
-# print(str(milk) + ' ml of milk')
-# print(str(beans) + ' g of coffee beans')
+k_cups = int(input("Write how many cups of coffee you will need:"))
+water = k_cups * coffee_water
+milk = k_cups * coffee_milk
+beans = k_cups * coffee_beans
+print('For ' + str(k_cups) + ' cups of coffee you will need:')
+print(str(water) + ' ml of water')
+print(str(milk) + ' ml of milk')
+print(str(beans) + ' g of coffee beans')
 
