682f47320aa206caea6594631ebd18b41c2cfc95
MaksBaskakov <m.a.baskakov@student.khai.edu>
2021-11-14 16:11:56 +0200

    3-st Stage

--- CoffeMachine/CoffeMachine.py
+++ CoffeMachine/CoffeMachine.py
@@ -8,12 +8,43 @@ Coffee is ready!""")
 coffee_water = 200
 coffee_milk = 50
 coffee_beans = 15
+water_m=int(input("Write how many water we got in machine"))
+milk_m=int(input("Write how many milk we got in machine"))
+beans_m=int(input("Write how many beans we got in machine"))
 k_cups = int(input("Write how many cups of coffee you will need:"))
-water = k_cups * coffee_water
-milk = k_cups * coffee_milk
-beans = k_cups * coffee_beans
-print('For ' + str(k_cups) + ' cups of coffee you will need:')
-print(str(water) + ' ml of water')
-print(str(milk) + ' ml of milk')
-print(str(beans) + ' g of coffee beans')
+cup_water = water_m // coffee_water
+cup_milk = milk_m // coffee_milk
+cup_beans = beans_m // coffee_beans
+number = min([cup_water, cup_milk, cup_beans])
+if number == k_cups:
+    print("Yes, I can make that amount of coffee")
+elif number > k_cups:
+    print("Yes, I can make that amount of coffee (and even " + str(number - k_cups) + " more than that)")
+else:
+    print("No, I can make only " + str(number) + " cups of coffee")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# water = k_cups * coffee_water
+# milk = k_cups * coffee_milk
+# beans = k_cups * coffee_beans
+# print('For ' + str(k_cups) + ' cups of coffee you will need:')
+# print(str(water) + ' ml of water')
+# print(str(milk) + ' ml of milk')
+# print(str(beans) + ' g of coffee beans')
 
