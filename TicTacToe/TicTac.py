1e951464884900496caf3220522c420b3d245fcf
MaksBaskakov <m.a.baskakov@student.khai.edu>
2021-12-13 09:15:30 +0200

    2 Stage

---
+++ TicTacToe/TicTac.py
@@ -0,0 +1,27 @@
+print("""    
+
+X0X
+0XX
+00X
+
+ """)
+
+print("Enter cells:")
+
+user_cells=list(input())
+
+def cells():
+
+
+
+    print('---------')
+    print('| ' + user_cells[0] + ' ' + user_cells[1] + ' ' + user_cells[2] + ' |')
+    print('| ' + user_cells[3] + ' ' + user_cells[4] + ' ' + user_cells[5] + ' |')
+    print('| ' + user_cells[6] + ' ' + user_cells[7] + ' ' + user_cells[8] + ' |')
+    print('---------')
+
+cells()
+
+
+
+
